849b2980ea1942355a2eeced259ad47527b06d35
DenebBoito <deneb.boito@gmail.com>
2022-03-25 12:09:07 +0100

    added function to compute dti invariants

--- qtiplus_fit.m
+++ qtiplus_fit.m
@@ -235,12 +235,7 @@ if pipeline < 8
 invariants = compute_invariants(model);
 else
     % compute only dti invatiants
-    siz = size(model);
-    mreshape = reshape(model, prod(siz(1:3)), 7);
-    D = mreshape(:,2:7) * 1e9;
-    invariants.s0 = squeeze(model(:,:,:,1));
-    [invariants.FA, invariants.AD, invariants.RD, invariants.ax_dir, invariants.FA_col] = compute_lamda_params(D,siz);
-    invariants.MD = 1/3 * sum(model(:,:,:,2:4),4) * 1e9; % change units to um2/ms
+    invariants = compute_dti_invariants(model);
 end
 fprintf('...Done!\n')
 
